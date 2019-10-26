981affc065a2dddb856782494fbfd4f5f0afc208
yusufdeepwork <57027351+yusufdeepwork@users.noreply.github.com>
2019-10-26 17:52:14 +0300

    Add files via upload

---
+++ ButunGun.java
@@ -0,0 +1,30 @@
+/**
+ * @author Yusuf
+ * @since 26.10.2019
+ */
+
+import java.util.Scanner;
+
+public class ButunGun {
+
+	public static void main(String[] args) {
+  Scanner tara = new Scanner(System.in);
+ System.out.println("Bir sayý giriniz");
+  int alinansayi = Math.abs(tara.nextInt());
+
+  for(int i = 1,b,toplam = 0; i<alinansayi;i++) {
+  b = i;
+  while(b>=10) {
+	  toplam = toplam + (b%10);
+	  b = b/10;
+  }
+  toplam = toplam + b ;
+  System.out.println("sayýnýz  : " + i + " basamaklarý toplamý : " + toplam );
+  toplam = 0;
+  }
+
+	
+	
+	
+	}
+}
